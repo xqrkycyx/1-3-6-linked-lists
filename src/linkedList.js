a32fd0f721d328466789f11a6162ac9e7f60fb9c
Will Williams <wwilliams@indeed.com>
2024-02-27 15:24:39 -0800

    Implements singly linked list

--- src/linkedList.js
+++ src/linkedList.js
@@ -20,18 +20,54 @@ class LinkedList {
    *   the number of elements in the linked list.
    */
 
-  get length() {}
+  get length() {
+    let result = 0;
+    let node = this.head;
+
+    while (node) {
+      result++;
+      node = node.next;
+    }
+    return result;
+  }
+
+  /**
+   * Find a node, and its previous node, in the linked list.
+   * @param isMatch
+   *  Function that returns `true` if the current node matches the search criteria.
+   *
+   * @returns {[Node|null, Node|null]}
+   *  The first element is the node where `isMatch(node, index) === true` or `null` if no match is found.
+   *  The second element is the previous node, or `null` if no match is found.
+   *  This second element is also `null` if `this.head` is the matched node.
+   */
+  findWithPrevious(isMatch) {
+    let index = 0;
+    let previous = null;
+    let node = this.head;
+    while (node) {
+      if (isMatch(node, index, this)) {
+        return [node, previous];
+      }
+      index++;
+      previous = node;
+      node = node.next;
+    }
+    return [null, null];
+  }
 
   /**
    * Find a node in the linked list.
    *
    * @param isMatch
-   *  function that returns true if the current node matches the search criteria.
+   *  Function that returns `true` if the current node matches the search criteria
    *
    * @returns {*|null}
-   *  the first node where `isMatch(node, index) === true` or null if no match is found.
+   *  The first node where `isMatch(node, index) === true`, or `null` if no match is found
    */
-  find(isMatch) {}
+  find(isMatch) {
+    return this.findWithPrevious(isMatch)[0];
+  }
 
   /**
    * Insert the value after a matched node in the list.
@@ -48,7 +84,20 @@ class LinkedList {
    * @throws 'No match found.'
    *  if list is not empty and no matching element is found.
    */
-  insert(value, isMatch = (node, index) => index === this.length - 1) {}
+  insert(value, isMatch = (node, index) => index === this.length - 1) {
+    if (this.head) {
+      const previousNode = this.find(isMatch);
+
+      if (!previousNode) {
+        throw new Error("No match found.");
+      }
+
+      previousNode.next = new Node(value, previousNode.next);
+    } else {
+      this.insertAtHead(value);
+    }
+    return this;
+  }
 
   /**
    * Insert a new value at the head of the list.
@@ -61,18 +110,34 @@ class LinkedList {
 
   insertAtHead(value) {
     // This is a new function that you will need to implement.
+    this.head = new Node(value, this.head);
+    return this;
   }
 
   /**
    * Remove the first node where `isMatch(node, index, this) === true`.
    *
    * @param isMatch
-   *  function that returns true if the current node matches the node to be removed.
+   *  Function that returns `true` if the current node matches the node to be removed
    *
    * @returns {*}
-   *  the value of the removed node, where `isMatch(node, index) === true` or null if no match is found.
+   *  The value of the removed node, where `isMatch(node, index) === true`, or `null` if no match is found
    */
-  remove(isMatch) {}
+
+  remove(isMatch) {
+    const [matchedNode, previousNode] = this.findWithPrevious(isMatch);
+
+    if (!matchedNode) {
+      return null;
+    }
+
+    if (this.head === matchedNode) {
+      this.head = this.head.next;
+    } else {
+      previousNode.next = matchedNode.next;
+    }
+    return matchedNode.value;
+  }
 }
 
 module.exports = LinkedList;
